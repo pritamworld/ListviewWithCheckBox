d78bf5dcac7f2db936c5f811fb5e9ca20af50008
pritesh.patel <pritam.world@gmail.com>
2017-08-23 14:08:21 -0400

    Text to Speech support added

--- app/src/main/java/com/pritesh/listviewwithcheckbox/rc/GCListActivity.java
+++ app/src/main/java/com/pritesh/listviewwithcheckbox/rc/GCListActivity.java
@@ -2,20 +2,26 @@ package com.pritesh.listviewwithcheckbox.rc;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.pritesh.listviewwithcheckbox.R;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class GCListActivity extends Activity
 {
     RecyclerView rvGCItem;
     ArrayList<GcItem>mGcItemArrayList;
     RCAdapter mRCAdapter;
+    Button btnSpeak;
+    TextToSpeech t1;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -25,6 +31,7 @@ public class GCListActivity extends Activity
         setData();
 
         rvGCItem = (RecyclerView)findViewById(R.id.rvGCItem);
+        btnSpeak = (Button)findViewById(R.id.btnSpeak);
 
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
         rvGCItem.setLayoutManager(gridLayoutManager);
@@ -40,6 +47,11 @@ public class GCListActivity extends Activity
                     @Override public void onItemClick(View view, int position) {
                         // do whatever
                         Log.d("RecyclerView", "onClick：" + position);
+                        Toast.makeText(GCListActivity.this, String.format(Locale.CANADA,"Click Position [ %d ]",position), Toast.LENGTH_SHORT).show();
+                        if(!t1.isSpeaking())
+                        {
+                            t1.speak(mGcItemArrayList.get(position).getTitle(), TextToSpeech.QUEUE_FLUSH, null);
+                        }
                     }
 
                     @Override public void onLongItemClick(View view, int position) {
@@ -47,11 +59,62 @@ public class GCListActivity extends Activity
                     }
                 })
         );
+
+        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.CANADA);
+                    t1.setSpeechRate(1f);
+                }
+            }
+        });
+
+        btnSpeak.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                String toSpeak = "My name is pritesh patel";
+                Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+                t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+    }
+
+    public void onPause()
+    {
+        if(t1 !=null){
+            t1.stop();
+            t1.shutdown();
+        }
+        super.onPause();
     }
 
     private void setData()
     {
         mGcItemArrayList = new ArrayList<>();
+        mGcItemArrayList.add(new GcItem(R.drawable.buy_green, 1 ,"BUY This Vehicle", true));
+        mGcItemArrayList.add(new GcItem(R.drawable.buy_green, 2 ,"PASS On This Vehicle", true));
+        mGcItemArrayList.add(new GcItem(R.drawable.buy_green, 3 ,"GUARANTEE Remaining Payments Only", true));
+        mGcItemArrayList.add(new GcItem(R.drawable.buy_green, 4 ,"GROUND Another Vehicle", true));
+        mGcItemArrayList.add(new GcItem(R.drawable.buy_green, 5 ,"Complete CA Lien Release", true));
+        mGcItemArrayList.add(new GcItem(R.drawable.buy_green, 6 ,"HOME", true));
+
+        mGcItemArrayList.add(new GcItem(R.drawable.buy_green, 1 ,"BUY This Vehicle", true));
+        mGcItemArrayList.add(new GcItem(R.drawable.buy_green, 2 ,"PASS On This Vehicle", true));
+        mGcItemArrayList.add(new GcItem(R.drawable.buy_green, 3 ,"GUARANTEE Remaining Payments Only", true));
+        mGcItemArrayList.add(new GcItem(R.drawable.buy_green, 4 ,"GROUND Another Vehicle", true));
+        mGcItemArrayList.add(new GcItem(R.drawable.buy_green, 5 ,"Complete CA Lien Release", true));
+        mGcItemArrayList.add(new GcItem(R.drawable.buy_green, 6 ,"HOME", true));
+
+        mGcItemArrayList.add(new GcItem(R.drawable.buy_green, 1 ,"BUY This Vehicle", true));
+        mGcItemArrayList.add(new GcItem(R.drawable.buy_green, 2 ,"PASS On This Vehicle", true));
+        mGcItemArrayList.add(new GcItem(R.drawable.buy_green, 3 ,"GUARANTEE Remaining Payments Only", true));
+        mGcItemArrayList.add(new GcItem(R.drawable.buy_green, 4 ,"GROUND Another Vehicle", true));
+        mGcItemArrayList.add(new GcItem(R.drawable.buy_green, 5 ,"Complete CA Lien Release", true));
+        mGcItemArrayList.add(new GcItem(R.drawable.buy_green, 6 ,"HOME", true));
+
         mGcItemArrayList.add(new GcItem(R.drawable.buy_green, 1 ,"BUY This Vehicle", true));
         mGcItemArrayList.add(new GcItem(R.drawable.buy_green, 2 ,"PASS On This Vehicle", true));
         mGcItemArrayList.add(new GcItem(R.drawable.buy_green, 3 ,"GUARANTEE Remaining Payments Only", true));
